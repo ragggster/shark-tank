0bb1aa19a1943b30918fa6745efac171ecc73e84
Minna Xiao <mxiao26@stanford.edu>
2017-05-26 01:36:24 -0700

    feat extract

--- minna.py
+++ minna.py
@@ -9,12 +9,12 @@ from os import listdir
 
 # https://github.com/tyiannak/pyAudioAnalysis/wiki/3.-Feature-Extraction
 
-def extract_mfcc_features():
-	[Fs, x] = audioBasicIO.readAudioFile("s8-e1-p1.wav")
+def extract_mfcc_features(filename):
+	[Fs, x] = audioBasicIO.readAudioFile(filename)
 	x = audioBasicIO.stereo2mono(x)
 	F = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs)
 
-	# F is a matrix of (34, 1940)
+	# F is a matrix of (34, 1940), for example
 	# N = 1940, where N is the number of short-term frames that fit into the input audio recording
 
 	mfcc = F[8:21,:] # 13 mfcc features
@@ -24,14 +24,25 @@ def extract_mfcc_features():
 	mfcc_max = np.amax(mfcc, axis=1)
 
 	mfcc_stats = np.concatenate((mfcc_mean, mfcc_var, mfcc_min, mfcc_max), axis=0)
-	print(mfcc_stats)
+	mfcc_stats = np.reshape(mfcc_stats, (1, mfcc_stats.shape[0]))
 	return mfcc_stats
 
 def createWeightMatrixFromPitches():
 	season = 4
 	pitch_dir = './audio-scraping/season%s-pitches' %(season)
 	wav_files = listdir(pitch_dir)
-	print(wav_files)
+	all_features = None
+	for wav in wav_files:
+		full_wav = pitch_dir + "/" + wav
+		print(full_wav)
+		feats = extract_mfcc_features(full_wav)
+		if all_features is not None:
+			all_features = np.concatenate((all_features, feats), axis=0)
+		else:
+			all_features = feats
+
+	print(all_features.shape)
+	return all_features
 
 
 #http://www.scipy-lectures.org/advanced/scikit-learn/
